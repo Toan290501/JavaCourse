351d4416ac313b6099ca0187106e1170c75e6d11
Toannd <toandepzai290501@gmail.com>
2024-03-09 19:42:07 +0700

    section 4,5 method in java

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,31 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int a = 10 ;
+        int b = 7 ;
+        int c = 5 ;
+        boolean d = true ;
+        if (d == true){
+            findHighestScore(a,b,c);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+    public static void findHighestScore(int a,int b, int c) {
+        if (a > b) {
+            if (a > c) {
+                System.out.println(a + " is the highest number");
+            } else if (a < c) {
+                System.out.println(c + " is the highest number");
+            }else {
+                System.out.println(a + " equal to " + c);
+            }
+        } else if (a < b) {
+            if (b > c) {
+                System.out.println(b + " is the highest score");
+            }else {
+                System.out.println(b + "equal to" + c);
+            }
         }
     }
 }
+
